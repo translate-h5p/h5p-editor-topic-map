891dcb14b3a0f54f5571dd76fe4001594e5c294a
Sindre Bøyum <sindre.boyum@gmail.com>
2021-09-10 14:36:42 +0200

    add topic map as an h5p editor widget

--- src/h5p/H5P.util.ts
+++ src/h5p/H5P.util.ts
@@ -1,4 +1,4 @@
 import { H5PObject, H5PEditorObject } from "../../H5P";
 
-export const H5P = (window as any).H5P as H5PObject;
-export const H5PEditor = (window as any).H5P as H5PEditorObject;
+export const H5P: H5PObject = (window as any).H5P ?? {};
+export const H5PEditor: H5PEditorObject = (window as any).H5PEditor ?? {};
