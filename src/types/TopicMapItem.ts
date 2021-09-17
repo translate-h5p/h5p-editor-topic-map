b082077c076dabce0f34c2b4038fcd50c9113f9f
Sindre Bøyum <sindre.boyum@gmail.com>
2021-09-17 09:50:39 +0200

    topic map item: rename `width` and `height` properties

--- src/types/TopicMapItem.ts
+++ src/types/TopicMapItem.ts
@@ -1,8 +1,15 @@
 export type TopicMapItem = {
   id: string;
+
+  /** The x position as a percentage of the container's width */
   xPercentagePosition: number;
+
+  /** The y position as a percentage of the container's height */
   yPercentagePosition: number;
 
-  xPercentageSize: number;
-  yPercentageSize: number;
+  /** The width as a percentage of the container's width */
+  widthPercentage: number;
+
+  /** The height as a percentage of the container's height */
+  heightPercentage: number;
 };
