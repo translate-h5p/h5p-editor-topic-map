e2500b3e0378021f070d5805666661bd26796f8b
Henriette Rudi Moe <henriette.moe@evry.com>
2021-09-23 10:44:54 +0200

    Add MapEditorView to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { hot } from "react-hot-loader/root";
+import { MapEditorView } from "./components/MapEditorView/MapEditorView";
 
 type AppProps = {
   adjective: string;
@@ -9,7 +10,7 @@ type AppProps = {
 const App = ({ adjective, setValue }: AppProps): JSX.Element => {
   return (
     <>
-      <h1>Hi, you&apos;re {adjective}</h1>
+      <MapEditorView />
     </>
   );
 };
