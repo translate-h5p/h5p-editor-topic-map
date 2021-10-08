f56fab7b93ac4b4661e1679e9c7ff202a9722fab
Sindre Bøyum <sindre.boyum@gmail.com>
2021-10-08 18:46:49 +0200

    create generic form for semantics

---
+++ src/components/SemanticsForm/SemanticsForm.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { H5PEditor } from "../../h5p/H5P.util";
+import { H5PField } from "../../types/h5p/H5PField";
+import { Params } from "../../types/h5p/Params";
+
+export type SemanticsFormProps = {
+  field: H5PField;
+  params: Params;
+  parent: H5PField;
+};
+
+export const SemanticsForm: React.FC<SemanticsFormProps> = ({
+  field,
+  params,
+  parent,
+}) => {
+  const wrapperRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    if (!wrapperRef.current) {
+      return;
+    }
+
+    const $wrapper = H5PEditor.$(wrapperRef.current);
+    H5PEditor.processSemanticsChunk(field, params, $wrapper, parent);
+  }, [field, params, parent, wrapperRef]);
+
+  return <div ref={wrapperRef} />;
+};
