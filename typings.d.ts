420168070dcfbced1f41918dac128f0c29ca7ea4
Sindre Bøyum <sindre.boyum@gmail.com>
2022-01-07 14:11:39 +0100

    improve CSS Modules typings

--- typings.d.ts
+++ typings.d.ts
@@ -1,2 +1,9 @@
-declare module "*.module.css";
-declare module "*.module.scss";
+declare module "*.module.css" {
+  const styles: { [className: string]: string};
+  export default styles;
+}
+
+declare module "*.module.scss" {
+  const styles: { [className: string]: string};
+  export default styles;
+}
