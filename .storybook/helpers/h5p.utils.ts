71099a9302caf04d2a3a2a5f6882a5f6ae6dcd5e
benq <beliy255@gmail.com>
2022-02-04 12:09:54 +0100

    change label to index in topic map item form

--- .storybook/helpers/h5p.utils.ts
+++ .storybook/helpers/h5p.utils.ts
@@ -178,7 +178,7 @@ export const semantics: H5PFieldGroup = {
             label: "Index",
             description:
               "⚠️ Advanced feature: Used for manually setting tab order.",
-            name: "label",
+            name: "index",
             type: H5PFieldType.Number,
             optional: true,
           },
