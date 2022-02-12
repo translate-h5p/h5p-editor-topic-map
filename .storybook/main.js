38cba7f032791ffa3f8ba9ba17ffd2723b775e42
boyum <sindre.boyum@tietoevry.com>
2022-02-12 17:33:39 +0100

    add theme picker in Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,11 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-addon-themes",
+  ],
   core: {
     builder: "webpack5",
   },
