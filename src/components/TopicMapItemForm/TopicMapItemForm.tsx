feb48cfd13feb72084dee5b979d261c7ad803d0a
Kent Inge F. Simonsen <kent.simonsen@tietoevry.com>
2022-04-01 15:21:49 +0200

    update arrows when boxes are updated

--- src/components/TopicMapItemForm/TopicMapItemForm.tsx
+++ src/components/TopicMapItemForm/TopicMapItemForm.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { H5PField, H5PFieldGroup } from "../../types/H5P/H5PField";
 import { H5PForm } from "../../types/H5P/H5PForm";
 import { Params } from "../../types/H5P/Params";
+import { getLabel } from "../../utils/arrow.utils";
 import { getTopicMapItemsField } from "../../utils/H5P/form.utils";
 import { SemanticsForm } from "../SemanticsForm/SemanticsForm";
 import "./TopicMapItemForm.scss";
@@ -51,12 +52,43 @@ export const TopicMapItemForm: React.FC<TopicMapItemFormProps> = ({
           item.id === updatedItem.id ? updatedItem : item,
         ) ?? [];
 
+      const unaffectedArrows =
+        params.arrowItems?.filter(arrow => {
+          return (
+            arrow.startElementId !== updatedItem.id &&
+            arrow.endElementId !== updatedItem.id
+          );
+        }) || [];
+
+      const affectedArrows =
+        params.arrowItems
+          ?.filter(arrow => {
+            return (
+              arrow.startElementId === updatedItem.id ||
+              arrow.endElementId === updatedItem.id
+            );
+          })
+          .map(arrow => {
+            return {
+              ...arrow,
+              label: getLabel(
+                arrow.startElementId,
+                arrow.endElementId,
+                arrow.arrowType,
+                updatedItems,
+              ),
+            };
+          }) || [];
+
+      const updatedArrows = [...unaffectedArrows, ...affectedArrows];
+
       onSave({
         ...newParams,
         topicMapItems: updatedItems,
+        arrowItems: updatedArrows,
       });
     },
-    [onSave, params.topicMapItems],
+    [onSave, params.arrowItems, params.topicMapItems],
   );
 
   return formParams && topicMapItemsField ? (
